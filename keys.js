ae1ed8ae84623f09e92ee3050f25203918c6daaf
Ernesto Rivas <64172275+ErnestoRivas99@users.noreply.github.com>
2020-05-15 03:11:44 -0500

    Dulces sueños... ¿ok?

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'sql3.freemysqlhosting.net',
+        user: 'sql3340763',
+        password: 'pYMGDBHxyF',
+        database: 'sql3340763'
+    }
+
+};
